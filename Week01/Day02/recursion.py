1b853a3540fef33c2c9f879783b84151c7390a80
1916378 <40574442+1916378@users.noreply.github.com>
2019-06-18 10:01:28 -0700

    recursion.py

---
+++ Week01/Day02/recursion.py
@@ -0,0 +1,16 @@
+def factorial (n) :
+    if n == 1:
+        return 1
+    return n * factorial (n - 1)
+
+def fib(n) :
+    if (n == 1) or (n == 0):
+          return n
+    return fib(n - 1) + fib(n - 2)
+
+def reverse_iter(tempString):
+    
+
+print (factorial (18))
+print (fib (19))
+
